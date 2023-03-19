bf148478a4eb5a4bb7cf9cdb7d8cc5a8f9c1060b
skuttenkuler <sam.kuttenk@gmail.com>
2023-03-19 11:31:22 -0500

    added IMDB link to cards

--- src/app/components/MovieCard/MovieCard.tsx
+++ src/app/components/MovieCard/MovieCard.tsx
@@ -1,14 +1,15 @@
 import React from "react"
+import { Link } from "react-router-dom";
 import { IMovie } from "../../models/movie";
 
 
 const MovieCard: React.FunctionComponent<IMovie> = (movie: IMovie) => {
     //console.log(movie)
     return (
-        <div>
+        <Link to={`https://www.imdb.com/title/${movie.imdbID}/?ref_=nv_sr_srsg_0`}>
             <img src={movie.Poster} alt={movie.Title}/>
             <p>{movie.Title}</p>
-        </div>
+        </Link>
     )
 };
 
