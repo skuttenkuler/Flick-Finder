e3cf01583f51fa3b487ac31c4dd452764eb1c220
skuttenkuler <sam.kuttenk@gmail.com>
2023-03-19 08:27:15 -0500

    basic clean up, added home and about pages, router working.

---
+++ src/app/pages/About/About.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+
+const AboutPage: React.FunctionComponent = () => {
+    return (
+        <div>AboutPage</div>
+    )
+};
+
+export default AboutPage;
---
+++ src/app/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+
+const HomePage: React.FunctionComponent = () => {
+    return (
+        <div>HomePage</div>
+    )
+};
+
+export default HomePage;
