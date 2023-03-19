087ab41643e5e27c924cc6cf757a53ba32f16731
skuttenkuler <sam.kuttenk@gmail.com>
2023-03-19 11:48:41 -0500

    added basic About page info

--- src/app/pages/About/About.tsx
+++ src/app/pages/About/About.tsx
@@ -4,7 +4,11 @@ import React from "react"
 
 const AboutPage: React.FunctionComponent = () => {
     return (
-        <div>AboutPage</div>
+        <div className="about-container">
+            <header>ABOUT</header>
+            <p>Flick Finder is a simple app that helps users find movies with links to their respective IMDB pages. Have a flick on your mind? We're here to help you find!</p>
+            {/* <a href="https://github.com/skuttenkuler/Flick-Finder"></a> */}
+        </div>
     )
 };
 
