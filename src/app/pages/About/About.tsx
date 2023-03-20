ad49f25e7d6f9f651ad677fb406af23ba4cca18f
skuttenkuler <sam.kuttenk@gmail.com>
2023-03-20 12:59:00 -0500

    about page adjusted and styled

--- src/app/pages/About/About.tsx
+++ src/app/pages/About/About.tsx
@@ -1,14 +1,14 @@
 import React from "react"
-
+import './styles.css'
 
 
 const AboutPage: React.FunctionComponent = () => {
     return (
-        <div className="about layout">
-            <div className="banner">
-                <h2>ABOUT</h2>
-                <p>Flick Finder is a simple app that helps users find movies with links to their respective IMDB pages. Have a flick on your mind? We're here to help you find!</p>
-            {/* <a href="https://github.com/skuttenkuler/Flick-Finder"></a> */}
+        <div className="about about_layout">
+                 <header>ABOUT</header>
+            <div className="about_banner">
+                <p className="about-text">Flick Finder is a simple movie search application written in React utilizing Typescript. Have a movie on your mind? Use the search on the home page to find the movie you are looking for. Want more details about the film? The IMDB page is merely a click away!</p>
+                <p className="about-link">Source Code can be found here: <a href="https://github.com/skuttenkuler/Flick-Finder">Github</a> </p>
             </div>
         </div>
     )
